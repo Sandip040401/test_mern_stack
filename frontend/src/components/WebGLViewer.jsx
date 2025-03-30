496ce43d00672a3bef555062472f63b1bd6db57e
Sandip040401 <iemsandip@gmail.com>
2025-03-30 23:21:36 +0530

    updates

--- frontend/src/components/WebGLViewer.jsx
+++ frontend/src/components/WebGLViewer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const WebGLViewer = () => {
+  const API_URL = import.meta.env.VITE_API_URL;
+
   return (
     <div className="w-full h-[80vh] border border-gray-300">
+      {/* Update this URL if deploying */}
       <iframe
-        src="http://localhost:5000/webgl/index.html" // Update this URL if deploying
+        src={`${API_URL}/webgl/index.html`}
         title="WebGL Viewer"
         className="w-full h-full"
       ></iframe>
