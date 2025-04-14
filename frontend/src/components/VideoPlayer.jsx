72121114d2c057730416a737317a945263bb1a41
Sandip040401 <iemsandip@gmail.com>
2025-04-14 15:15:57 +0530

    updates

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -62,7 +62,7 @@ const VideoPlayer = () => {
     formData.append("video", selectedFile);
   
     // Initialize WebSocket connection
-    const newSocket = io("ws://localhost:5000");
+    const newSocket = io("ws://test-mern-stack-6jv4.onrender.com");
     setSocket(newSocket);
   
     newSocket.on("uploadProgress", (data) => {
